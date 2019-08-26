8e9c417103a0ca398701e7fe1586cfc661f54567
Taynan <taynacid123@gmail.com>
2019-08-26 20:26:21 -0300

    link sequelize and create a user

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'Hello asdasd' }));
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'taynan',
+    email: 'taynacid123@gmail.com',
+    password_hash: '123123',
+  });
+
+  res.json(user);
+});
 
 module.exports = routes;
